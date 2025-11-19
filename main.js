9119da64898ca024a766caf72fefc6d167ecf2de
minhtc <2053300+minhtc@users.noreply.github.com>
2025-11-19 12:08:14 +0700

    refactor: detect the OS and assign the correct User-Agent string dynamically

--- main.js
+++ main.js
@@ -28,24 +28,28 @@ try {
   console.error('Failed to load mode', e)
 }
 
-const FIREFOX_USER_AGENT =
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:145.0) Gecko/20100101 Firefox/145.0'
-
-const getChromeUserAgent = () => {
-  const version = '142.0.0.0'
+const getFirefoxUserAgent = () => {
   if (process.platform === 'darwin') {
-    // macOS
-    return `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${version} Safari/537.36`
+    return 'Macintosh; Intel Mac OS X 10.15'
   } else if (process.platform === 'win32') {
-    // Windows
-    return `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${version} Safari/537.36`
+    return 'Windows NT 10.0; Win64; x64'
   } else {
-    // Linux
-    return `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${version} Safari/537.36`
+    return 'X11; Linux x86_64'
   }
 }
 
-const CHROME_USER_AGENT = getChromeUserAgent()
+const getChromeUserAgent = () => {
+  if (process.platform === 'darwin') {
+    return `Macintosh; Intel Mac OS X 10_15_7`
+  } else if (process.platform === 'win32') {
+    return `Windows NT 10.0; Win64; x64`
+  } else {
+    return `X11; Linux x86_64`
+  }
+}
+
+const FIREFOX_USER_AGENT = `Mozilla/5.0 (${getFirefoxUserAgent()}; rv:145.0) Gecko/20100101 Firefox/145.0`
+const CHROME_USER_AGENT = `Mozilla/5.0 (${getChromeUserAgent()}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36`
 
 let mainWindow
 let tray
